a3cb10b4ec3fe275f746f2366d9ff08f07daf46a
curry <499608316@qq.com>
2024-07-14 00:56:43 +0800

    master-Demon1 init

---
+++ src/com/atguigu/gitStudy/Demon1.java
@@ -0,0 +1,16 @@
+package com.atguigu.gitStudy;
+
+/**
+ * ClassName: Demon1
+ * Package: com.atguigu.gitStudy
+ * Description:
+ *
+ * @Author: Zhang Peiyuan
+ * @Create: - 0:55
+ * @Version: v1.0
+ */
+public class Demon1 {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
