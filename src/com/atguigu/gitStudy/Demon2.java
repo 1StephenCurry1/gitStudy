14551b57a53276406de1efd1d8de2e22f6820473
curry <499608316@qq.com>
2024-07-14 00:58:24 +0800

    feature1-Demon2- init

---
+++ src/com/atguigu/gitStudy/Demon2.java
@@ -0,0 +1,16 @@
+package com.atguigu.gitStudy;
+
+/**
+ * ClassName: Demon2
+ * Package: com.atguigu.gitStudy
+ * Description:
+ *
+ * @Author: Zhang Peiyuan
+ * @Create: - 0:57
+ * @Version: v1.0
+ */
+public class Demon2 {
+    public int sub(int a, int b) {
+        return a - b;
+    }
+}
